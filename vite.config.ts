c2a00356259f3b1157d451e8e608f42e3e868795
gabino <jose.murielsanchez@plexus.es>
2026-04-28 11:17:22 +0200

    🔥FIX_polyfills due lack of buffer

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+      },
+    })
   ],
   resolve: {
     alias: {
